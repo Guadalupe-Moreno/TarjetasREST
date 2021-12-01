a27eba329898331bd1434418f5c5436f129806ec
Guadalupe-Moreno <ing.guadalupe.moreno@gmail.com>
2021-11-30 22:08:15 -0600

    Agrego PostgreSQL

--- src/main/java/com/ibm/academia/apirest/entities/Cliente.java
+++ src/main/java/com/ibm/academia/apirest/entities/Cliente.java
@@ -4,6 +4,13 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.ibm.academia.apirest.enums.Preferencia;
 
 import lombok.AllArgsConstructor;
@@ -17,11 +24,21 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table (name = "clientes", schema = "tarjeta")
 public class Cliente implements Serializable
 {
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column (name = "edad", unique = true, nullable = false, length = 50)
 	private Integer edad;
+	
+	@Column (name = "sueldo", unique = true, nullable = false, length = 50)
 	private BigDecimal sueldo;
+	
+	@Column (name = "preferencia", unique = true, nullable = false, length = 50)
 	private Preferencia preferencia;
 	
 	@Override
